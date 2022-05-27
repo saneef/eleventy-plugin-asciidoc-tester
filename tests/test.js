d06de98ef52116dbab8a7bcca0036c0a3dce394c
saneef <hello@saneef.com>
2022-05-27 22:08:32 +0530

    Add test for front matter permalink

--- tests/test.js
+++ tests/test.js
@@ -2,12 +2,18 @@ const path = require("path");
 const fs = require("fs");
 const test = require("ava").default;
 
-const filePath = path.resolve(__dirname, "../dist/index.html");
-const data = fs.readFileSync(filePath, "utf8");
+const indexFilePath = path.resolve(__dirname, "../dist/index.html");
+const indexData = fs.readFileSync(indexFilePath, "utf8");
+
+const permaLinkedFilePath = path.resolve(
+  __dirname,
+  "../dist/hello-world/index.html"
+);
+const permaLinkedData = fs.readFileSync(permaLinkedFilePath, "utf8");
 
 test("Has <title> populated", async (t) => {
   const titleRegex = /<title>(?<title>.+)<\/title>/m;
-  const res = titleRegex.exec(data);
+  const res = titleRegex.exec(indexData);
 
   const title = res?.groups?.["title"];
 
@@ -18,12 +24,21 @@ test("Has <title> populated", async (t) => {
 test("Has content populated", async (t) => {
   const regex = /<p>A preface about/m;
 
-  t.truthy(data.match(regex));
+  t.truthy(indexData.match(regex));
 });
 
 test("Has code transformed for syntax highlighting", async (t) => {
   const regex =
     /<pre class="language-ruby"><code class="language-ruby">puts <span class="token string-literal"><span class="token string">"Hello, World!"<\/span><\/span><\/code><\/pre>/m;
 
-  t.truthy(data.match(regex));
+  t.truthy(indexData.match(regex));
+});
+
+test("Permalink in front matter is processed", async (t) => {
+  const titleRegex = /<title>(?<title>.+)<\/title>/m;
+  const res = titleRegex.exec(permaLinkedData);
+
+  const title = res?.groups?.["title"];
+
+  t.is(title, "Hello World");
 });
