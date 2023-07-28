157a09c191776892cc1ce7dee825e242bd19b2bf
Saneef Ansari <hello@saneef.com>
2023-07-28 14:46:47 +0530

    bump eleventy-plugin-asciidoc to v3.1.0

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,12 @@
 const eleventyAsciidoc = require("eleventy-plugin-asciidoc");
 const shoutExt = require("./asciidoc-extensions/shout.js");
 
-const asciidoctor = require("asciidoctor").default();
-const registry = asciidoctor.Extensions.create();
-
-shoutExt.register(registry);
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyAsciidoc, {
     template_dir: `${__dirname}/asciidoc-templates`,
-    extension_registry: registry,
+    configure_extension_registry: (registry) => {
+      shoutExt.register(registry);
+    },
   });
 
   return {
