4e8c17e220964e9547c981b7062d97634cbf2b84
Saneef Ansari <hello@saneef.com>
2026-07-07 10:30:17 +0530

    test: check kroki svg generation

--- tests/extensions.js
+++ tests/extensions.js
@@ -2,6 +2,7 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import test from "ava";
+import { glob } from "node:fs/promises";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,4 +15,10 @@ test("'shout' block extension processed content", async (t) => {
   const res = contentRegex.exec(htmlData);
 
   t.truthy(res);
+});
+
+test("'kroki' block extension should fetch a svg", async (t) => {
+  const dir = path.join(__dirname, "..", "dist", "mermaid/");
+  const files = await Array.fromAsync(glob("*.svg", { cwd: dir }));
+  t.assert(files.length > 0);
 });
